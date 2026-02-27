a3f14aac030e7fd1e0364ebb6e9f4301ea454ec6
Leo <leonid112@yandex.ru>
2026-02-27 09:47:20 +0300

    mysql add

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
 from flask import Flask
 from flask import render_template
 from flask import request
+import pymysql.cursors
 
 app = Flask(__name__)
 
+conn = pymysql.connect(host='127.0.0.1',
+                       user='pk31',
+                       password='1234',
+                       database='pk31_processors')
+cur = conn.cursor()
+
+
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
-    if request.method == 'POST':
-        name = request.form.get('mlp_name')
-        mlp_chk = request.form.getlist('chk')  # безопаснее
-        print(mlp_chk, name)
-    return render_template('index.html')
+    cur.execute('SELECT model, manufacturer, price FROM processors')
+    ans = cur.fetchall()
+    print(ans)
+    return render_template('index.html', ans=ans)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
