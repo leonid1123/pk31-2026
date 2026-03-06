6430b9420ed37450f668938c8e37179233980a1e
Leo <leonid112@yandex.ru>
2026-03-06 09:09:48 +0300

    фильтр

--- main.py
+++ main.py
@@ -12,15 +12,43 @@ conn = pymysql.connect(host='127.0.0.1',
 cur = conn.cursor()
 
 
+def get_manufacturers():
+    man_list = []
+    cur.execute("SELECT DISTINCT manufacturer FROM processors")
+    ans = cur.fetchall()
+    for item in ans:
+        man_list.append(item[0])
+    return man_list
+
+
 @app.route("/", methods=['GET', 'POST'])
 def main_page():
+    ans = None
     if request.method == 'POST': #нужно поискать в словаре
-        btn1 = request.form['sort_up']
-        btn2 = request.form['sort_down']
-        print(btn1, btn2)
-    cur.execute('SELECT model, manufacturer, price FROM processors')
-    ans = cur.fetchall()
-    return render_template('index.html', ans=ans)
+        if 'sort_up' in request.form:
+            print('по возрастанию')
+            cur.execute('SELECT model, manufacturer, price FROM processors ORDER BY price DESC')
+            ans = cur.fetchall()
+        if 'sort_down' in request.form:
+            print('по убыванию')
+            cur.execute('SELECT model, manufacturer, price FROM processors ORDER BY price ASC')
+            ans = cur.fetchall()
+        if 'filter' in request.form:
+            man_selected = request.form.get('manufacturers')
+            sql_values = (man_selected,)
+            cur.execute('''SELECT model, manufacturer, price 
+                FROM processors WHERE manufacturer = %s ''',
+                        sql_values)
+            ans = cur.fetchall()
+
+    else:
+        cur.execute('SELECT model, manufacturer, price FROM processors')
+        ans = cur.fetchall()
+    return render_template('index.html',
+                           ans=ans,
+                           man=get_manufacturers())
+
+# сделать кнопку "сбросить всё"
 
 
 if __name__ == "__main__":
